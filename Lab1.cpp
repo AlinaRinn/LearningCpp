a6629ca767ae635c95bf09c8a6364102cc0ff1c4
Nikita Lu <nik17042001@outlook.com>
2021-09-07 18:17:57 +0300

    Finish

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,8 +1,22 @@
 ﻿#include <iostream>
+#include <ctime>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n = 0, m = 0, i = 0, sum = 0;
+    cout << "Enter value for 'n' and 'm':" << endl;
+    cin >> n >> m;
+    srand(time(0));
+     do {
+        i++;
+        sum = 0;
+        while (sum < n) {
+            sum += rand() % 10 + 1;
+            //cout << "prom: " << sum << endl;
+        }   
+     } while (!(sum % m == 0));
+     cout << "Summary: " << sum << endl;
+     cout << "Tries: " << i << endl;
+    system("pause");
 }
-
-// "Ы" commit
