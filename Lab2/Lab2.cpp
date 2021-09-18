244a5902aa1e379b9397376c9792753d40286c1e
Nikita Lu <nik17042001@outlook.com>
2021-09-18 19:18:42 +0300

    multiplication hotfix

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -6,32 +6,18 @@ using namespace std;
 void multiplication(double** A, double** B, double** C, int N) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            C[i][j] = A[i][j]*B[j][i];          
+            for (int k = 0; k < N; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+            }
         }
     }
 }
 
-void print(double** A, double** B, double** C, int N) {
+void print(double** A, int N) {
     cout << endl << endl;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             cout << setw(5) << A[i][j];
-        }
-        cout << endl;
-    }
-    cout << endl << endl;
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cout << setw(5) << B[i][j];
-        }
-        cout << endl;
-    }
-    cout << endl << endl;
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cout << setw(5) << C[i][j];
         }
         cout << endl;
     }
@@ -41,7 +27,8 @@ void print(double** A, double** B, double** C, int N) {
 int main()
 {
     int N = 1;
-    cout << "Enter array size:\n"; cin >> N;
+    cout << "Enter array size:\n"; 
+    cin >> N;
     srand(time(0));
 
     //array init
@@ -54,16 +41,23 @@ int main()
         C[i] = new double[N];
     }
 
-    //randommm
+    //random
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             A[i][j] = rand() % 10 + 1;
             B[i][j] = rand() % 10 + 1;
+            C[i][j] = 0;
         }
     }
 
+    //func
     multiplication(A, B, C, N);
-    print(A, B, C, N);
+    cout << "Array A: ";
+    print(A, N);
+    cout << "Array B: ";
+    print(B, N);
+    cout << "Array C: ";
+    print(C, N);
     
     //clear mem
     for (int i = 0; i < N; i++) {
