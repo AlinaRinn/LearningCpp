d9e30bd53f460dff992abf9f9efdbebd5e9ee672
Nikita Lu <nik17042001@outlook.com>
2021-09-28 16:30:58 +0300

    lab4 top

---
+++ Lab4/Lab4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+class Cat{
+public:
+    string name, type;
+    int playаbility = 44;
+
+    void Play(){
+        if (playаbility > 50) {
+
+        }
+    }
+    void Sleep(){
+
+    }
+};
+
+class Dog : public Cat {
+    int playаbility = 88;
+};
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
