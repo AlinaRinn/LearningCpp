ff60457b3b5a9768e3c4cc8d3cabd61cd066c122
Nikita Lu <nik17042001@outlook.com>
2021-10-26 16:55:31 +0300

    start

---
+++ Lab6/Lab6.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+class ShootingField {
+
+};
+
+class Target {
+    int Radius;
+};
+
+class BigTarget : Target {
+
+};
+
+class MediumTarget : Target {
+
+};
+
+class SmallTarget : Target {
+
+};
+
+
+
+int main()
+{
+
+
+    std::cout << "Hello World!\n";
+}
