8543228bc763857cb23a9a8cb523fdcd30d09326
Nikita Lu <nik17042001@outlook.com>
2022-09-24 18:43:09 +0300

    Initial commit lab 1

---
+++ Lab1 parallel/Lab1 parallel.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <thread>
+
+void hunter(int _id)
+{
+	std::cout << "Launched by thread " << _id << std::endl;
+}
+
+int main()
+{
+	std::cout << "Duck hunt" << std::endl;
+	const int num_threads = 10;
+	std::thread thr(num_threads);
+	for (int i = 0; i < num_threads; ++i)
+	{
+		thr[i] = std::thread(function_to_call, i);
+	}
+
+	for (int i = 0; i < num_threads; ++i)
+	{
+		thr[i].join;
+	}
+	return 0;
+}
