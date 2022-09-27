a821a04f3fda997e4574c6b46560c9b0c2a1845e
Nikita Lu <nik17042001@outlook.com>
2022-09-27 22:45:21 +0300

    mid commit

--- Lab1 parallel/Lab1 parallel.cpp
+++ Lab1 parallel/Lab1 parallel.cpp
@@ -1,24 +1,71 @@
-﻿#include <iostream>
-#include <thread>
+﻿// 22. «Утиная охота». Вероятность подстрелить утку составляет 1%. Произвести 1000 (суммарно, или в каждом потоке?)
+// выстрелов по утке, используя 1, 2 или 4 охотников(потоки). Подсчитать число
+// попаданий в утку в каждом из трех случаев.
 
-void hunter(int _id)
+#include <iostream>
+#include <thread>
+#include <vector>
+
+void hunter(int &hit, int shoots)
 {
-	std::cout << "Launched by thread " << _id << std::endl;
+	srand((unsigned int)time(0));
+	for (int i = 0; i < shoots; i++) {
+		int random = rand() % 100;
+		if (random == 7) {
+			std::cout << "Bang! Gotcha! \n";
+			hit++;
+		}
+		else {
+			std::cout << "Bang! Miss \n";
+		}
+	}
 }
 
 int main()
 {
-	std::cout << "Duck hunt" << std::endl;
-	const int num_threads = 10;
-	std::thread thr(num_threads);
-	for (int i = 0; i < num_threads; ++i)
-	{
-		thr[i] = std::thread(function_to_call, i);
-	}
+	int hits = 0;
+	int shoots = 1000;
+	int hunters = 1;
+	std::vector<std::thread> threads;
 
-	for (int i = 0; i < num_threads; ++i)
+	std::cout << "     __\n" <<
+		           " ____(o)>\n" <<
+		            "\\ < _. )\n" <<
+		             " `----'\n" << "\nDuck hunt" << std::endl;
+
+	for (int i = 0; i < hunters; i++)
 	{
-		thr[i].join;
+		threads.push_back(std::thread(hunter, std::ref(hits), shoots));
 	}
+	for (auto & th : threads)
+	{
+		th.join();
+	}
+	std::cout << "---------\nHits: " << hits << " Hunters: " << threads.size() << "\n\n\n";
+	threads.clear();
+	hunters++;
+
+	for (int i = 0; i < hunters; i++)
+	{
+		threads.push_back(std::thread(hunter, std::ref(hits), shoots));
+	}
+	for (auto& th : threads)
+	{
+		th.join();
+	}
+	std::cout << "---------\nHits: " << hits << " Hunters: " << threads.size() << "\n\n\n";	threads.clear();
+	hunters += 2;
+
+	for (int i = 0; i < hunters; i++)
+	{
+		threads.push_back(std::thread(hunter, std::ref(hits), shoots));
+	}
+	for (auto& th : threads)
+	{
+		th.join();
+	}
+	std::cout << "---------\nHits: " << hits << " Hunters: " << threads.size() << "\n\n\n";
+	threads.clear();
+
 	return 0;
 }
