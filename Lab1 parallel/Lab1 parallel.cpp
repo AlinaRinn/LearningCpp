95a70ec758ebfb3fe736479b6bee943596582ce3
Nikita Lu <nik17042001@outlook.com>
2022-10-04 22:33:53 +0300

    The time is now (now time count correctly)

--- Lab1 parallel/Lab1 parallel.cpp
+++ Lab1 parallel/Lab1 parallel.cpp
@@ -6,56 +6,48 @@
 #include <thread>
 #include <vector>
 #include <mutex>
+#include <chrono>
 std::mutex mutex;
 
-void hunter(int &hit, int shoots, double &total_time)
+void hunter(int &hit, int shoots)
 {
-	double time_thread = 0;
-	clock_t start, end;
 	int local_hit = 0;
-	start = clock();
-
 	srand((unsigned int)time(0));
+
 	for (int i = 0; i < shoots; i++) {
 		int random = rand() % 100;
 		if (random == 7) {
-			//std::cout << "Bang! Gotcha! \n";
 			local_hit++;
 		}
-		else {
-			//std::cout << "Bang! Miss \n";
-		}
 	}
-	end = clock();
-	time_thread = ((double)end - start) / ((double)CLOCKS_PER_SEC);
+
 	mutex.lock();
-	total_time += time_thread;
 	hit += local_hit;
 	mutex.unlock();
-	std::cout << "Hunter's time: " << time_thread << "\n";
 }
 
 void create_threads(int hunters, int shoots) {
 	int hits = 0;
 	int thread_shoots = shoots / hunters;
-	double total_time = 0;
 	std::vector<std::thread> threads;
-
+	auto start = std::chrono::high_resolution_clock::now();
 	for (int i = 0; i < hunters; i++)
 	{
-		threads.push_back(std::thread(hunter, std::ref(hits), thread_shoots, std::ref(total_time)));
+		threads.push_back(std::thread(hunter, std::ref(hits), thread_shoots));
 	}
 	for (auto& th : threads)
 	{
 		th.join();
 	}
-	std::cout << "-----------------\nHits: " << hits << " Hunters: " << threads.size() << " Total time: " << total_time << "\n\n\n";
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<float> duration = end - start;
+	std::cout << "-----------------\nHits: " << hits << " Hunters: " << threads.size() << " Total time: " << (float)duration.count() << "\n\n\n";
 	threads.clear();
 }
 
 int main()
 {
-	int shoots = 10000000; // Must be /2
+	int shoots = 100000000; // Must be /2
 	int hunters = 1;
 
 	std::cout << "     __\n" <<
@@ -63,9 +55,9 @@ int main()
 		            "\\ < _. )\n" <<
 		             " `----'\n" << "\nDuck hunt" << std::endl;
 	create_threads(hunters, shoots);
-	hunters += 3;
+	hunters += 1;
 	create_threads(hunters, shoots);
-	hunters += 4;
+	hunters += 2;
 	create_threads(hunters, shoots);
 
 	system("pause");
