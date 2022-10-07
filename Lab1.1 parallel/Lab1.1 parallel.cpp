36c36d9b7c198ed286d30583656e9c9e5cd9f557
Nikita Lu <nik17042001@outlook.com>
2022-10-07 23:48:25 +0300

    near to final commit (count fixes)

--- Lab1.1 parallel/Lab1.1 parallel.cpp
+++ Lab1.1 parallel/Lab1.1 parallel.cpp
@@ -17,31 +17,33 @@ std::mutex mutex;
 std::array<std::string, 300> custom_array;
 
 
-void counter(int start, int stop) {
+void counter(int start, int stop, int *even_array) {
 	std::string s;
 	int even = 0, x = 0;
-	for (auto i : custom_array) {
-		s = i;
+	for (int i = start; i <= stop; i++) {
+		s = custom_array.at(i);
 		for (auto c : s) {
 			x = c - '0';
 			if (x % 2 == 0) {
 				even++;
 			}
 		}
+		even_array[i] = even;
+		even = 0;
 	}
 }
 
 float create_threads(int number_threads, int string_lenght) {
-	int even = 0,
-		threads_piece = (int)(custom_array.size() / number_threads),
+	int threads_piece = (int)(custom_array.size() / number_threads),
 		start = 0,
 	    stop = threads_piece - 1;
+	std::array<int, custom_array.size()> even_array;
 	std::vector<std::thread> threads;
 	auto begin = std::chrono::high_resolution_clock::now();
 
 	for (int i = 0; i < number_threads; i++)
 	{
-		threads.push_back(std::thread(counter, start, stop));
+		threads.push_back(std::thread(counter, start, stop, &even_array[0]));
 		start = stop + 1;
 		stop += threads_piece;
 	}
@@ -52,7 +54,9 @@ float create_threads(int number_threads, int string_lenght) {
 
 	auto end = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<float> duration = end - begin;
-	std::cout << "\n-----------------\nEven: " << even << " Odd: " << string_lenght - even << std::endl;
+	for (int i = 0; i < even_array.size() ; i++) {
+		std::cout << std::left << "String: " << std::setw(8) << i << " Even: " << std::setw(7) << even_array.at(i) << " Odd: " << std::setw(7) << string_lenght - even_array.at(i) << "\n";
+	}
 	//threads.clear();
 	return (float)duration.count();
 }
@@ -61,10 +65,10 @@ int main()
 {
 	// Variables
 	int number_threads = 1, 
-		string_lenght = 20;
+		string_lenght = 200000;
 	srand((unsigned int)time(0));
 
-	// Filling Array
+	// Filling Array. Also need parallelization for better performance -_-
 	float progress = 0, 
 		increment = 100.0 / custom_array.size();
 	std::cout << "Start of filling array\n";
@@ -73,18 +77,18 @@ int main()
 			custom_array.at(i) += std::to_string((rand() % 9));
 		}
 		progress += increment;
-		std::cout << std::left << std::setprecision(3) << std::setw(5) << progress << "%\n";
+		std::cout << std::left << std::setprecision(3) << std::setw(6) << progress << "%\n";
 	}
-	std::cout << "Finish of filling array\n";
+	std::cout << "Finish of filling array\n\n";
 
-	// Threads
-	std::cout << "Threads: " << number_threads << "Total time: " << create_threads(number_threads, string_lenght);
+	// Threads & time output
+	std::cout << "\nThreads: " << number_threads << " Total time: " << create_threads(number_threads, string_lenght) << "\n\n\n";
 	number_threads++;
-	std::cout << "Threads: " << number_threads << "Total time: " << create_threads(number_threads, string_lenght);
+	std::cout << "\nThreads: " << number_threads << " Total time: " << create_threads(number_threads, string_lenght) << "\n\n\n";
 	number_threads += 2;
-	std::cout << "Threads: " << number_threads << "Total time: " << create_threads(number_threads, string_lenght);
+	std::cout << "\nThreads: " << number_threads << " Total time: " << create_threads(number_threads, string_lenght) << "\n\n\n";
 	number_threads += 6;
-	std::cout << "Threads: " << number_threads << "Total time: " << create_threads(number_threads, string_lenght);
+	std::cout << "\nThreads: " << number_threads << " Total time: " << create_threads(number_threads, string_lenght) << "\n\n\n";
 
 	system("pause");
 	return EXIT_SUCCESS;
